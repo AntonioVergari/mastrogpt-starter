2ab9aca3f23d8ae093c694437467ac967b5dd928
Antonio Vergari <14364501+AntonioVergari@users.noreply.github.com>
2025-02-20 06:53:50 +0000

    feat: complete exercise 3

--- packages/form/puzzle/puzzle.py
+++ packages/form/puzzle/puzzle.py
@@ -2,6 +2,29 @@ import re, os, requests as req
 #MODEL = "llama3.1:8b"
 MODEL = "phi4:14b"
 
+FORM = [
+  {
+    "name": "queen",
+    "label": "Add a queen",
+    "type": "checkbox",
+  },
+  {
+    "name": "bishop",
+    "label": "Add bishops",
+    "type": "checkbox",
+  },
+  {
+    "name": "tower",
+    "label": "Add towers",
+    "type": "checkbox",
+  },
+  {
+    "name": "knight",
+    "label": "Add knights",
+    "type": "checkbox",
+  },
+]
+
 def chat(args, inp):
   host = args.get("OLLAMA_HOST", os.getenv("OLLAMA_HOST"))
   auth = args.get("AUTH", os.getenv("AUTH"))
@@ -23,15 +46,25 @@ def puzzle(args):
   out = "If you want to see a chess puzzle, type 'puzzle'. To display a fen position, type 'fen <fen string>'."
   inp = args.get("input", "")
   res = {}
+  print(f"primo:{inp}")
   if inp == "puzzle":
     inp = "generate a chess puzzle in FEN format"
-    out = chat(args, inp)
-    fen = extract_fen(out)
-    if fen:
-       print(fen)
-       res['chess'] = fen
-    else:
-      out = "Bad FEN position."
+    res["form"] = FORM
+    handle_puzzle_gen(args, inp, res)
+  # Adding form data check
+  elif type(inp) is dict and "form" in inp:
+    form_data = inp["form"]
+    inp = "generate a chess puzzle in FEN format."
+    if form_data["queen"] == "true":
+      inp += "add a queen on each side."
+    if form_data["knight"] == "true":
+      inp += "add max two knight on each side."
+    if form_data["tower"] == "true":
+      inp += "add max two a tower on each side."
+    if form_data["bishop"] == "true":
+      inp += "add max two a bishop on each side."
+    print(inp)
+    handle_puzzle_gen(args, inp, res)
   elif inp.startswith("fen"):
     fen = extract_fen(inp)
     if fen:
@@ -46,3 +79,13 @@ def puzzle(args):
 
   res["output"] = out
   return res
+
+
+def handle_puzzle_gen(args:dict, inp:str, res:dict):
+  out = chat(args, inp)
+  fen = extract_fen(out)
+  if fen:
+      print(fen)
+      res['chess'] = fen
+  else:
+    out = "Bad FEN position."
